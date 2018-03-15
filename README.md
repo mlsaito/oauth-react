58a6972d1f3833def427691d2c245671a621ac57
Makoto Saito <mlsaito@live.com>
2018-03-15 18:48:00 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Social Login Button using ReactJS
+
+## Description
+
+![Sign in with Github](https://dha4w82d62smt.cloudfront.net/items/370W1f0h3P1W1g2V1i0l/Image%202018-03-15%20at%204.46.42%20PM.png)
+
+A social login button example using ReactJS.
+
+This repo uses an OAuth provider called [OAuth.io](https://oauth.io/) which provides 100+ OAuth providers such as Twitter, Facebook, Google, and a lot more. They have SDKs available as well for JavaScript, NodeJS, PHP, iOS, Android, as per their documentation.
+
+## Running on Local
+
+```
+$ git clone https://github.com/mlsaito/oauth-react
+
+// Install yarn package manager if you don't have it yet
+$ yarn start
+```
+
+## How-to
+
+Check how-to here: https://coderwall.com/p/wtub1q/social-login-button-for-oauth-using-reactjs
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+
+class App extends Component {
+
+  // Downloads oauth.js from CDN, pretty much like adding external scripts
+  componentDidMount () {
+    const oauthScript = document.createElement("script");
+    oauthScript.src = "https://cdn.rawgit.com/oauth-io/oauth-js/c5af4519/dist/oauth.js";
+
+    document.body.appendChild(oauthScript);
+  }
+
+  handleClick(e) {
+    // Prevents page reload
+    e.preventDefault();
+
+    // Initializes OAuth.io with API key
+    // Sign-up an account to get one
+    window.OAuth.initialize('HlyhAkZq67D9cU6dnAkHnNwW1Ho');
+
+    // Popup facebook and ask for authorization
+    window.OAuth.popup('github').then((provider) => {
+    
+      // Prompts 'welcome' message with User's name on successful login
+      // Check console logs for additional User info
+      provider.me().then((data) => {
+        console.log("data: ", data);
+        alert("Welcome " + data.name + "!");
+      });
+
+    });
+  }
+
+  render() {
+    return <a href="" onClick={this.handleClick.bind(this)} className="btn btn-social btn-github">
+             <span className="fa fa-github"></span> Sign in with Github
+           </a>;
+  }
+}
+
+export default App;
